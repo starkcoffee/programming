0f9517089df70615a70259840ec5a63cc1e8529b
Duana Saskia <duana@soundcloud.com>
2013-06-19 09:58:35 +0200

    adding reverse challenge

---
+++ reverse.rb
@@ -0,0 +1,11 @@
+# modify this function so that it returns a list in reverse order
+# goal is when you run `ruby reverse.rb`, you should see the numbers
+# printed in reverse
+
+def reverse(list)
+  return list
+end
+
+list = [1,2,3]
+reversed_list = reverse(list)
+puts reversed_list
