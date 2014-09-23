ad49a81cbc7db2729c2cae67f7230e52bf317569
Duana Saskia <duana@soundcloud.com>
2014-09-23 15:05:09 +0200

    Passing functions to functions.

--- javascript/functions/functions.js
+++ javascript/functions/functions.js
@@ -1,25 +1,44 @@
 
-var example_text = [
+var introduction = [
   ["The", "quick", "brown", "fox"],
   ["jumped", "over", "the", "rabbit"],
 ];
 
+var legal_terms = [
+  ["You", "must", "not", "copy", "this"]
+];
+
 // text should be an array of arrays containing words
-function style_paragraph(text) {
+// returns a the styled text as a string
+function style_paragraph(lines, style_word) {
   var styled_text = "";
 
-  for(i=0; i<text.length; i++){
-
-    for(j=0; j< text[i].length; j++){
-      styled_text += text[i][j];
+  for(i=0; i<lines.length; i++){
+    var line = lines[i];
+    for(j=0; j< line.length; j++){
+      var word = line[j];
+      styled_text += " " +  style_word(word);
     } 
   }
+ 
+  styled_text = styled_text.substring(1, styled_text.length);
 
   return styled_text;
 }
 
-var result = style_paragraph(example_text);
+function uppercase_word(word){
+  return word.toUpperCase();
+}
+
+function downcase_word(word){
+  return word.toLowerCase(word);
+}
+
+var result = style_paragraph(introduction, downcase_word);
+result+= "\n";
+result+= style_paragraph(legal_terms, uppercase_word);
 
 console.log(result);
+
 
 
